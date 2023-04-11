4219f08cc3fbb0f887d4d2aeaff14c0a2237d47a
Josef Fussell <josef.k.fussell@gmail.com>
2023-04-11 02:19:00 -0500

    test

---
+++ src/main/java/com/joseffussell/joseffussellportfolio/JosefFussellPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.joseffussell.joseffussellportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosefFussellPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosefFussellPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseffussell/joseffussellportfolio/JosefFussellPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseffussell.joseffussellportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosefFussellPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
